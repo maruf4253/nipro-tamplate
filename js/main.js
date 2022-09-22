f7b89d2f1fd82665f317b27f2c660411053f3665
MD MARUF BILLAH <110770179+maruf4253@users.noreply.github.com>
2022-09-22 15:31:44 +0600

    template

---
+++ js/main.js
@@ -0,0 +1,42 @@
+  // Gallery Filter JQuery Codes
+  var $grid = $('.grid').isotope({
+
+  });
+  
+  // filter items on button click
+  $('.gallery-btn-area').on( 'click', 'li', function() {
+    $(this).addClass('active').siblings().removeClass('active');
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
+
+  $('.grid').isotope({
+    itemSelector: '.gallery-item',
+    percentPosition: true,
+    masonry: {
+    // use outer width of grid-sizer for columnWidth
+    columnWidth: '.gitem'
+    }
+  });
+
+
+   $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        navText: ["PREV", "NEXT"],
+        dots:true,
+        autoplay:true,
+        autoplayTimeout:10000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
